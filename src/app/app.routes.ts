b0117cd943e3d6c3fe4b73170c6ce2502b5859b1
SanthyCarloNoto <santhy472@gmail.com>
2024-07-20 11:38:58 +0200

    actualizacion cv

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { CvComponent } from './cv/cv.component';
-import { ContactoComponent } from './contacto/contacto.component';
+import { HomeComponent } from './home/home.component'; // Asegúrate de que la ruta sea correcta
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'cv', component: CvComponent },
-  { path: 'contacto', component: ContactoComponent }
+  { path: '', component: HomeComponent }, // Ruta para la página principal
+  // Puedes añadir más rutas aquí según lo necesites
 ];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,29 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+// src/main.ts
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { HomeComponent } from './app/home/home.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// Define las rutas
+const routes: Routes = [
+  { path: '', component: HomeComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch(err => console.error(err));
