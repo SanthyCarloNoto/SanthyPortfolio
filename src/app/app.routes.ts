90703b307cfac6c30c2e9f52f0bed270db3d3743
SanthyCarloNoto <santhy472@gmail.com>
2024-11-27 21:31:08 +0100

    actualizacion 2711

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component'; // Asegúrate de que la ruta sea correcta
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent }, // Ruta para la página principal
-  // Puedes añadir más rutas aquí según lo necesites
-];
